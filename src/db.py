780f966c29f621bb65eb4901f14e1b2e5d3efd0e
swannyen <hengswanen@u.nus.edu>
2026-04-12 01:00:37 +0800

    feat: save by dataframe

--- src/db.py
+++ src/db.py
@@ -1,11 +1,12 @@
 import sqlite3
 import pandas as pd
+from src.date_utils import SAVED_DATE_FORMAT
 
 DB_PATH = "portfolio.db"
 
 class SQLiteDB:
     def __init__(self):
-        self.conn = sqlite3.connect(DB_PATH)
+        self.conn = sqlite3.connect(DB_PATH, check_same_thread=False)
         self.cursor = self.conn.cursor()
 
     def ingest_excel(self, df:pd.DataFrame, table_name: str):
@@ -14,29 +15,6 @@ class SQLiteDB:
 
     def get_table(self, table_name: str) -> pd.DataFrame:
         return pd.read_sql(f"SELECT * FROM {table_name}", self.conn)
-
-    def init_db(self):
-        self.cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS transactions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                date TEXT,
-                portfolio TEXT,
-                ticker TEXT,
-                asset_name TEXT,
-                asset_class TEXT,
-                currency TEXT,
-                action TEXT,
-                quantity REAL,
-                value REAL,
-                value_base REAL,
-                price_per_unit REAL,
-                year INTEGER
-            )
-            """
-        )
-
-        self.conn.commit()
 
     def insert_transactions_from_df(self, df: pd.DataFrame):
         """
@@ -47,34 +25,34 @@ class SQLiteDB:
             self.cursor.execute(
                 """
                 INSERT INTO transactions (
-                    date,
-                    portfolio,
-                    ticker,
-                    asset_name,
-                    asset_class,
-                    currency,
-                    action,
-                    quantity,
-                    value,
-                    value_base,
-                    price_per_unit,
-                    year
+                    Date,
+                    Portfolio,
+                    Ticker,
+                    "Asset Name",
+                    "Asset Class",
+                    Currency,
+                    Action,
+                    Quantity,
+                    Value,
+                    "Value (base)",
+                    "Price per Unit",
+                    Year
                 )
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 """,
                 (
-                    str(pd.to_datetime(row["Date"]).date()) if pd.notna(row["Date"]) else None,
-                    row["Portfolio"],
-                    row["Ticker"],
-                    row["Asset Name"],
-                    row["Asset Class"],
-                    row["Currency"],
-                    row["Action"],
-                    row["Quantity"],
-                    row["Value"],
-                    row["Value (base)"],
-                    row["Price per Unit"],
-                    int(row["Year"]) if pd.notna(row["Year"]) else None,
+                    pd.to_datetime(row.get("Date")).strftime(SAVED_DATE_FORMAT) if pd.notna(row.get("Date")) else None,
+                    row.get("Portfolio"),
+                    row.get("Ticker"),
+                    row.get("Asset Name"),
+                    row.get("Asset Class"),
+                    row.get("Currency"),
+                    row.get("Action"),
+                    row.get("Quantity"),
+                    row.get("Value"),
+                    row.get("Value (base)"),
+                    row.get("Price per Unit"),
+                    int(row.get("Year")) if pd.notna(row.get("Year")) else None,
                 ),
             )
 
@@ -84,3 +62,4 @@ class SQLiteDB:
         self.conn.close()
 
 
+db = SQLiteDB()
